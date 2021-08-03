db5ac272113fc9345f7fa0e27c86ea4b399669d9
Sanjay45 <sanjay>
2021-08-03 18:31:38 +0530

    initial commit

---
+++ src/main/kotlin/com/designpattern/carpicker/Main.kt
@@ -0,0 +1,19 @@
+package com.designpattern.carpicker
+
+
+//In kotlin
+fun main() {
+    println("Hello World")
+}
+
+
+//In java
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World")
+        }
+    }
+}
